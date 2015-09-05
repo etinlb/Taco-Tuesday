ecc83eee62245e48c6f7bf93a760bf385cb2484c
Erik Parreira <etinlb@gmail.com>
2015-09-05 16:26:49 -0700

    Initial Commit

---
+++ Taco TuesdayTests/Taco_TuesdayTests.swift
@@ -0,0 +1,36 @@
+//
+//  Taco_TuesdayTests.swift
+//  Taco TuesdayTests
+//
+//  Created by Erik Parreira on 9/5/15.
+//  Copyright © 2015 Erik Parreira. All rights reserved.
+//
+
+import XCTest
+@testable import Taco_Tuesday
+
+class Taco_TuesdayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
